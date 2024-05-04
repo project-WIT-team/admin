74abe28ec0b6745e77bc1af9b722739b5ac1b9fd
Showlee <showlee912@outlook.com>
2024-05-04 21:12:47 +0800

    goodsTable图片更新

--- src/components/goodsTable/goodsTable.vue
+++ src/components/goodsTable/goodsTable.vue
@@ -23,7 +23,9 @@
             <el-table-column prop="" label="图片">
                 <template #="scope">
                     <el-image style="width: 100px; height: 100px" zoom-scale="1.2"
-                        :src="`http://8.149.133.241:5868${scope.row.mainImage}`" fit="cover"></el-image>
+                        :src="`http://8.149.133.241:5868${scope.row.mainImage}`"
+                        :preview-src-list="scope.row.topImage.map((image: any) => `http://8.149.133.241:5868${image.path}`)"
+                        :zoom-rate="1.2" fit="cover" preview-teleported></el-image>
                 </template>
             </el-table-column>
             <!-- 下架 -->
@@ -75,8 +77,20 @@
     import { ElMessageBox } from 'element-plus'
     import { ElNotification } from 'element-plus'
     import { onMounted } from 'vue';
+    import { computed } from 'vue';
+
 
     const goodsStore = useGoodsListStore()
+
+    // const srcList = [
+    //     'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
+    //     'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
+    //     'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
+    //     'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
+    //     'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
+    //     'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
+    //     'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg',
+    // ]
 
 
 
@@ -215,9 +229,7 @@
 </script>
 
 <style lang="scss" scoped>
-    .table {
-        display: inline-block
-    }
+
 
     .pagination {
         display: flex;
