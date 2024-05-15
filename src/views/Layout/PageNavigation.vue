dd0d37bbc3ed4df2257ecb7e9c8f06425d727d4c
lishuo <showlee912@outlook.com>
2024-05-15 19:19:41 +0800

    添加navigation的icon和文字

--- src/views/Layout/PageNavigation.vue
+++ src/views/Layout/PageNavigation.vue
@@ -15,45 +15,64 @@
 
 <template>
     <nav class="app-topnav">
-        <div class="container">
-            <ul>
-                <!-- 条件渲染:1.登陆状态 -->
-                <template v-if="userStore.userInfo">
-                    <li class="no-bullet">
-                        <el-popconfirm title="确认退出吗?" @confirm="confirm()" confirm-button-text="确认"
-                            cancel-button-text="取消">
-                            <template #reference>
-                                <a href="javascript:" class="no-underline"> <el-icon>
-                                        <Avatar />
-                                    </el-icon> 退出登录</a>
-                            </template>
-                        </el-popconfirm>
-                    </li>
-                </template>
-                <!-- 2.未登陆状态 -->
-                <template v-else>
-                    <li>
-                        <a href="javascript:" @click="$router.push('/login')">请先登录</a>
-                    </li>
-
-                </template>
-            </ul>
+        <div class="iconDiv">
+            <img height="30px" src="@/assets/icon/store-manage.png" alt="icon">
+            <p>商城后台管理系统</p>
         </div>
+        <ul>
+            <!-- 条件渲染:1.登陆状态 -->
+            <template v-if="userStore.userInfo">
+                <li>
+                    <el-popconfirm title="确认退出吗?" @confirm="confirm()" confirm-button-text="确认" cancel-button-text="取消">
+                        <template #reference>
+                            <a href="javascript:"> <el-icon>
+                                    <Avatar />
+                                </el-icon> 退出登录</a>
+                        </template>
+                    </el-popconfirm>
+                </li>
+            </template>
+            <!-- 2.未登陆状态 -->
+            <template v-else>
+                <li>
+                    <a href="javascript:" @click="$router.push('/login')">请先登录</a>
+                </li>
+
+            </template>
+        </ul>
+
     </nav>
 </template>
 
 
 <style scoped lang="scss">
-    .no-bullet {
+    .iconDiv {
+        margin-left: 30px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+
+        p {
+            margin-left: 10px;
+            color: white;
+        }
+    }
+
+
+    li {
         list-style-type: none;
     }
 
-    .no-underline {
+    a {
         text-decoration: none;
     }
 
     .app-topnav {
         background: #333;
+        display: flex;
+        flex-direction: row;
+        height: 50px;
 
         ul {
             display: flex;
